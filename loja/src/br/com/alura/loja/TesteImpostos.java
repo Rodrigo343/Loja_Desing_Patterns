eeda1f02cd6416d6b6fdc87b8898edc7fe6215af
Rodrigo Ramos de Freitas <rodrigoramos2002@outlook.com>
2023-09-15 08:25:48 -0300

    Criação do projeto e aplicação do padrão strategy

---
+++ loja/src/br/com/alura/loja/TesteImpostos.java
@@ -0,0 +1,18 @@
+package br.com.alura.loja;
+
+import br.com.alura.loja.imposto.CalculadoraDeImpostos;
+import br.com.alura.loja.imposto.ICMS;
+import br.com.alura.loja.imposto.ISS;
+import br.com.alura.loja.imposto.Imposto;
+import br.com.alura.loja.orcamento.Orcamento;
+
+import java.math.BigDecimal;
+
+public class TesteImpostos {
+
+    public static void main(String[] args){
+        Orcamento orcamento = new Orcamento(new BigDecimal("100"));
+        CalculadoraDeImpostos calculadora= new CalculadoraDeImpostos();
+        System.out.println(calculadora.calcular(orcamento, new ICMS()));
+    }
+}
